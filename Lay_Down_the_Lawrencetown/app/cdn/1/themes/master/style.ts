759a9f81e9d4d3417feee4773c1630c0c92ea98a
Alex Smith <120223871+alexksmith@users.noreply.github.com>
2026-04-21 11:27:07 -0400

    Update style.ts

--- Lay_Down_the_Lawrencetown/app/cdn/1/themes/master/style.ts
+++ Lay_Down_the_Lawrencetown/app/cdn/1/themes/master/style.ts
@@ -2,8 +2,10 @@ import type { ThemeComponentStyleOptions } from 'jimu-theme'
 
 export const CssBaseline: ThemeComponentStyleOptions['CssBaseline'] = {
   root: () => ({
-    .jimu-alert-severity-warning {
+    html: {
+      .jimu-alert-severity-warning {
   visibility: hidden !important; 
   display: none !important;}
+    }
   })
 }
